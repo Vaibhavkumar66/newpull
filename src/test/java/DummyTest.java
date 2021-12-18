40458c02ef0486a21d142107b3417bbb40b5204e
Singh, Atin <atin@pragra.co>
2021-12-18 12:30:56 -0500

    updated for groups

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,9 +1,17 @@
 import org.testng.annotations.*;
 
 public class DummyTest {
+
+
+
     @BeforeSuite
     public void setUp() {
         System.out.println("Doing Setup on Suite");
+    }
+
+    @BeforeTest
+    public void setUp2() {
+        System.out.println("*******RUNNING BEFORE TEST******");
     }
 
     @Test
@@ -14,5 +22,19 @@ public class DummyTest {
     @AfterSuite
     public void tearDown() {
         System.out.println("Tearing down on Suite");
+    }
+
+    @AfterTest
+    public void tearDown2() {
+        System.out.println("******* AFTER TEST ********");
+    }
+
+    @BeforeClass
+    public void setUp23() {
+        System.out.println("Doing for " + getClass().getName());
+    }
+    @AfterClass
+    public void setUp24() {
+        System.out.println("Completed for " + getClass().getName());
     }
 }
--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,8 +1,37 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeTest {
-    @Test
+
+    @Test(groups = "sanity", timeOut = 2000)
+    public void sanity() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("SANITY");
+
+    }
+
+    @Test(groups = {"integration", "regression"})
     public void testName() {
         System.out.println("TC2");
+    }
+    @Test(groups = "regression", dependsOnGroups = "sanity")
+    public void testName2() {
+        System.out.println("TC3");
+    }
+    @Test(groups = {"regression"},dependsOnGroups = "sanity")
+    public void testName3() {
+        System.out.println("TC4");
+    }
+    @Test(groups = {"integration"}, dependsOnGroups = "sanity")
+    public void testName4() {
+        System.out.println("TC5");
+    }
+    @Test(groups = "regression", dependsOnGroups = "sanity")
+    public void testName5() {
+        System.out.println("TC6");
     }
 }
